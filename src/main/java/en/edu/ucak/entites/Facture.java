1b2d3d0b76c84bd55181c01fe3e1e590bd18870a
bara-samb <maambaara56@gmail.com>
2026-07-12 12:29:33 +0000

    facture done

--- src/main/java/en/edu/ucak/entites/Facture.java
+++ src/main/java/en/edu/ucak/entites/Facture.java
@@ -4,8 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalDateTime;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -16,4 +17,24 @@ public class Facture implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String numero;
+
+    @Column(nullable = false)
+    private LocalDateTime dateFacture;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @OneToMany(mappedBy = "facture", cascade = CascadeType.ALL)
+    private Set<LigneArticle> ligneArticles;
+
+    @PrePersist
+    public void prePersist() {
+        if (dateFacture == null) {
+            dateFacture = LocalDateTime.now();
+        }
+    }
 }
